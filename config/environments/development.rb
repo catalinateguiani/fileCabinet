d106dea6b80e880ffc6b6117cff0b2299d966207
Cata001 <catalinateguiani@yahoo.com>
2019-07-17 20:57:30 +0100

    Styling and uploading the image files

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,3 +59,4 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   # config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 end
+config.public_file_server.enabled = true
